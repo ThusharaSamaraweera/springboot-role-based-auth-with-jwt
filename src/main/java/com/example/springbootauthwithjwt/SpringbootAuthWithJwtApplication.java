975cfdcac9395017442755a31ad3824b9a7ffd4d
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-04-06 09:28:16 +0530

    init project

---
+++ src/main/java/com/example/springbootauthwithjwt/SpringbootAuthWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootauthwithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthWithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootauthwithjwt/SpringbootAuthWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootauthwithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
